5eef59d351d1de6fab1664beb96386bce8459b57
rylancole <rylancole33@hotmail.com>
2021-03-01 08:17:54 -0800

    Show repo name, post icon assests

--- src/components/RepoIcon/RepoIcon.tsx
+++ src/components/RepoIcon/RepoIcon.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
-
 import "./styles.scss";
 
+// import manage from './icons';
+
+// const grow = require("../../assets/grow.png")
+
 const RepoIcon: React.FC<{ repoName: string }> = ({ repoName }) => {
-    let iconName = "fa-github"
+    // let iconName = "fa-github"
 
-    if (repoName == "themis") { iconName = "fa-suitcase" }
+    // if (repoName == "themis") { iconName = "fa-suitcase" }
 
-    if (repoName == "grow") { iconName = "fa-line-chart" }
+    // if (repoName == "grow") { iconName = "fa-line-chart" }
 
-    return <i className={`fa ${iconName} fa-lg`} aria-hidden="true"></i>
+    // return <i className={`fa ${iconName} fa-lg`} aria-hidden="true"></i>
+
+    return <p>{repoName}</p>
 };
 
 export default RepoIcon;
